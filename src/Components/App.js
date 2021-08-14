ef633db7895479d226bb73a879a36cb362d90cf2
toan95dn <51420692+toan95dn@users.noreply.github.com>
2021-08-13 18:55:36 -0700

    .

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Navbar from "./Navbar";
+
+import AppStyle from "./AppStyle.scss";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+import NavbarStyle from "./NavbarStyle.scss";
+import { BsInfoCircle, BsCodeSlash, BsPhone } from "react-icons/bs";
+import {
+  AiOutlineInfoCircle,
+  AiOutlineCode,
+  AiOutlineContacts,
+  AiOutlineGithub,
+  AiOutlineLinkedin,
+  AiOutlineFacebook,
+} from "react-icons/ai";
+
+const Navbar = () => {
+  return (
+    <header>
+      <h2>Toan Pham</h2>
+      <nav>
+        <ul>
+          <li>
+            <AiOutlineInfoCircle className="icons" />
+            About Me
+          </li>
+          <li>
+            <AiOutlineCode className="icons" />
+            Porfolio
+          </li>
+          <li>
+            <AiOutlineContacts className="icons" />
+            Contact
+          </li>
+        </ul>
+      </nav>
+
+      <nav>
+        <ul>
+          <li>
+            <AiOutlineGithub className="icons" />
+          </li>
+          <li>
+            <AiOutlineLinkedin className="icons" />
+          </li>
+          <li>
+            <AiOutlineFacebook className="icons" />
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
