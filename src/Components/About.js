e5129ea3368f4ce10876a61340373d86af4bf57e
toan95dn <51420692+toan95dn@users.noreply.github.com>
2021-08-14 16:46:28 -0700

    add styling for about page

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,23 +1,46 @@
 import React from "react";
 import AboutStyle from "./AboutStyle.scss";
 import Avatar from "./Images/Me.jpg";
+import Typewriter from "typewriter-effect";
+import GraphemeSplitter from "grapheme-splitter";
 
 const About = () => {
-  //   const MyImage = new Image();
-  //   MyImage.src = Avatar;
+  const currStringSplitter = (string) => {
+    const splitter = new GraphemeSplitter();
+    return splitter.splitGraphemes(string);
+  };
 
   return (
     <div className="aboutContainer">
       <img src={Avatar} alt="" />
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio
-        sint, eius inventore repellat tenetur amet assumenda accusantium magni
-        illo est odit dolorem adipisci libero laudantium doloremque atque
-        facilis eaque optio. Accusamus sed minus repellendus suscipit et,
-        quaerat accusantium optio eveniet, qui facere neque veritatis
-        repudiandae. Blanditiis adipisci aspernatur amet labore perferendis!
-        Dolorum repellendus quos suscipit nemo animi doloremque labore porro!
-      </p>
+      <div>
+        <div className="typeWriter">
+          Hi, I am a&nbsp;
+          <Typewriter
+            options={{
+              loop: true,
+              stringSplitter: currStringSplitter,
+            }}
+            onInit={(typewriter) => {
+              typewriter
+                .typeString("<strong>CS student 👨‍🎓</strong>")
+                .pauseFor(1200)
+                .deleteAll()
+                .typeString("<strong>coder 💻</strong>")
+                .pauseFor(1200)
+                .deleteAll()
+                .typeString("<strong>guitar player 🎸</strong>")
+                .pauseFor(1200)
+                .start();
+            }}
+          />
+        </div>
+        I am currently a first year transfer student at UC Sandiego. I am
+        looking for an internship to improve my knowledge in software and web
+        development. Besides studying at school, I like to expand my knowledge
+        by doing side projects. Outside of coding, I love playing the guitar and
+        working out.
+      </div>
     </div>
   );
 };
