9806899ff6503382209b57a907cb483dc76c63c8
toan95dn <51420692+toan95dn@users.noreply.github.com>
2021-08-16 21:01:27 -0700

    add card styles

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -7,7 +7,7 @@ const Contact = () => {
     <iframe
       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3313.1134187776665!2d-118.3429046848337!3d33.86096778065838!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzPCsDUxJzM5LjUiTiAxMTjCsDIwJzI2LjYiVw!5e0!3m2!1sen!2sus!4v1629161756733!5m2!1sen!2sus"
       width="3000"
-      height="250"
+      height="200"
       frameBorder="0"
       style={{ border: 0 }}
       allowFullScreen=""
