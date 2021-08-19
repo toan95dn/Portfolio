0082877c75bb49ea3c11bc8bee0db63cc50b2e37
toan95dn <51420692+toan95dn@users.noreply.github.com>
2021-08-19 09:36:15 -0700

    try new colors

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -80,11 +80,23 @@ const About = () => {
         <div>
           <h3>About Me</h3>
           <p>
-            Hi, my name is Toan. I am currently a computer science student at UC
-            San Diego. I am looking for an internship to improve my knowledge in
-            software and web development. Besides studying at school, I like to
-            expand my knowledge by doing side projects. Outside of coding, I
-            love playing the guitar and working out.
+            Hi, my name is Toan (But everyone calls me Duc, which is my middle
+            name, because of a mistake in my visa when I immigrated to the US).
+            I am currently a computer science student at UC San Diego. Besides
+            studying at school, I love to expand my knowledge by doing side
+            projects. <br /> <br />I can design and build web/desktop
+            applications. I love to make my app interactive, responsive,
+            structured, and my code quality high. Thus, sometimes, I spend lots
+            of time fixing details and thinking about optimizing my code.
+            <br />
+            <br /> Now, I am looking for an internship to gain more experience
+            in software development as well as team collaboration skills. I am
+            curious and in love with solving problems. So, getting adapted to a
+            new working environment with new technology will not be a problem.
+            <br /> <br />
+            Outside of coding, I love playing the guitar, especially
+            fingerstyle. My favorite artist is Tommy Emanuel. Also, I work out
+            every day to maintain a good lifestyle.
           </p>
         </div>
 
